c8eb292085650da9a875dfaa25800f14d4f9be02
rafammassa <rafaella_massa@hotmail.com>
2024-10-11 09:40:23 -0300

    Criando a classe Pessoa

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using DesafioSistemaHospedagem.Models;
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Paloma Machado");
+Pessoa p2 = new Pessoa(nome: "Eduardo Folco");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
